3b946092257babb98f53d2dbe3c0bf904ef57160
Olivia <ojmay92@gmail.com>
2019-03-18 15:03:14 -0700

    assignment completed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 function onReady() {
+  let id = 0;
   const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
 
@@ -8,8 +9,11 @@ function onReady() {
 
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id
     });
+
+    id++
 
     newToDoText.value = '';
 
@@ -24,18 +28,30 @@ function onReady() {
     toDos.forEach(function(toDo){
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
+      const deleteItem = document.createElement('button')
+
       checkbox.type = "checkbox";
+      deleteItem.type = "button";
 
       newLi.textContent = toDo.title;
+      deleteItem.textContent = "Delete";
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteItem);
     });
   }
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
+  });
+
+  deleteItem.addEventListener('click', event => {
+    toDos = toDos.filter(function(el) {
+      return el.id !== toDo.id;
+    });
+    renderTheUI();
   });
 
   renderTheUI();
