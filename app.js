41818861966a532e657486a59aa9aeb6d00fd548
Olivia <ojmay92@gmail.com>
2019-03-19 19:14:29 -0700

    assignment complete

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 function onReady() {
   let id = 0;
-  const toDos = [];
+  localStorage.getItem("toDo");
+  var toDos = JSON.parse(localStorage.getItem("toDo"));
+  if (toDos == null) {
+    toDos = [];
+  }
   const addToDoForm = document.getElementById('addToDoForm');
 
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
-    if (!newToDoText.value) { return; }
+    if (!newToDoText.value) {
+      alert("Input can not be empty");
+       return; }
 
     toDos.push({
       title: newToDoText.value,
@@ -16,8 +22,10 @@ function onReady() {
     id++
 
     newToDoText.value = '';
+    newToDoText.focus();
 
     renderTheUI();
+    localStorage.setItem("toDo", JSON.stringify(toDos));
   }
 
   function renderTheUI() {
@@ -26,32 +34,57 @@ function onReady() {
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo){
+      let title = document.createElement('div');
+      title.textContent = toDo.title;
+      title.className = 'mdl-list_item-primary-content';
       const newLi = document.createElement('li');
+      newLi.className = 'mdl-list_item';
+
       const checkbox = document.createElement('input');
-      const deleteItem = document.createElement('button')
-
       checkbox.type = "checkbox";
-      deleteItem.type = "button";
+      checkbox.className = 'mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect';
 
-      newLi.textContent = toDo.title;
-      deleteItem.textContent = "Delete";
+      if (toDo.complete == true) {
+        checkbox.check = true;
+      } else if (toDo.complete == false) {
+        checkbox.check = false;
+      }
+
+      function toggle() {
+        if (checkbox.checked) {
+          toDo.complete = true;
+        } else {
+          toDo.complete = false;
+        }
+
+        localStorage.setItem("toDo", JSON.stringify(toDos));
+      }
+      checkbox.addEventListener('click', toggle);
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = "Delete";
+      deleteButton.className =  'mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary';
+
+      function deleteLi() {
+        toDos = toDos.filter(function(todo){
+          return toDo.id != todo.id;
+        });
+        renderTheUI();
+        localStorage.setItem("toDo", JSON.stringify(toDos));
+      }
+      deleteButton.addEventListener('click', deleteLi);
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
-      newLi.appendChild(deleteItem);
+      newLi.appendChild(title);
+      newLi.appendChild(deleteButton);
+
     });
   }
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
-  });
-
-  deleteItem.addEventListener('click', event => {
-    toDos = toDos.filter(function(el) {
-      return el.id !== toDo.id;
-    });
-    renderTheUI();
   });
 
   renderTheUI();
